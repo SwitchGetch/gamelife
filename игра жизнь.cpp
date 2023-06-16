bf3238d92e36dea8610f820037c9d8ca370e29d3
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2023-06-16 15:51:06 +0300

    Add files via upload

---
+++ игра жизнь.cpp
@@ -0,0 +1,250 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <windows.h>
+using namespace std;
+
+HANDLE hand = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD cord;
+
+void FieldCreation(vector<string>& field, int x, int y)
+{
+    for (int i = 0; i < x; i++)
+    {
+        string temp = "";
+
+        for (int j = 0; j < y; j++)
+        {
+            if (!i || i == x - 1 || !j || j == y - 1)
+            {
+                temp += '#';
+            }
+            else
+            {
+                if (!(rand() % 4))
+                {
+                    temp += '*';
+                }
+                else
+                {
+                    temp += ' ';
+                }
+            }
+        }
+
+        field.push_back(temp);
+    }
+
+    return;
+}
+
+void FieldOutput(vector<string>& field, int x, int y)
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            if (field[i][j] == '#')
+            {
+                SetConsoleTextAttribute(hand, 11);
+            }
+            else
+            {
+                SetConsoleTextAttribute(hand, 12);
+            }
+
+            cout << field[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void NewGeneration(vector<string>& field, vector<int> B, vector<int> S, int x, int y)
+{
+    vector<pair<int, int>> change;
+
+    for (int i = 1; i < x - 1; i++)
+    {
+        for (int j = 1; j < x - 1; j++)
+        {
+            int temp = 0;
+
+            for (int i0 = i - 1; i0 < i + 2; i0++)
+            {
+                for (int j0 = j - 1; j0 < j + 2; j0++)
+                {
+                    if (field[i0][j0] == '*')
+                    {
+                        temp++;
+                    }
+                }
+            }
+
+            if (field[i][j] == '*')
+            {
+                temp--;
+
+                bool death = true;
+
+                for (int i0 = 0; i0 < S.size(); i0++)
+                {
+                    if (temp == S[i0])
+                    {
+                        death = false;
+
+                        break;
+                    }
+                }
+
+                if (death)
+                {
+                    change.push_back({ i, j });
+                }
+            }
+            else
+            {
+                for (int i0 = 0; i0 < B.size(); i0++)
+                {
+                    if (temp == B[i0])
+                    {
+                        change.push_back({ i, j });
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < change.size(); i++)
+    {
+        if (field[change[i].first][change[i].second] == '*')
+        {
+            field[change[i].first][change[i].second] = ' ';
+        }
+        else
+        {
+            field[change[i].first][change[i].second] = '*';
+        }
+    }
+}
+
+void func()
+{
+    for (int i = 1; i < 16; i++)
+    {
+        SetConsoleTextAttribute(hand, i);
+        cout << i << endl;
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    vector<string> field;
+    vector<int> B, S;
+    string temp;
+    int GenCount = 0, x, y;
+
+    SetConsoleTextAttribute(hand, 15);
+
+    cout << "B: ";
+    cin >> temp;
+
+    for (int i = 0; i < temp.size(); i++)
+    {
+        if (temp[i] < 58 && temp[i] > 47)
+        {
+            B.push_back(temp[i] - '0');
+        }
+    }
+
+    cout << "S: ";
+    cin >> temp;
+
+    for (int i = 0; i < temp.size(); i++)
+    {
+        if (temp[i] < 58 && temp[i] > 47)
+        {
+            S.push_back(temp[i] - '0');
+        }
+    }
+
+    cout << "field (file name / random): ";
+    cin >> temp;
+
+    if (temp == "random")
+    {
+        cout << "length: ";
+        cin >> x;
+
+        cout << "width: ";
+        cin >> y;
+
+        FieldCreation(field, x, y);
+    }
+    else
+    {
+        ifstream ifile;
+        string line;
+
+        ifile.open(temp);
+
+        while (getline(ifile, line))
+        {
+            field.push_back(line);
+        }
+
+        ifile.close();
+
+        x = field.size();
+        y = field[0].size();
+    }
+
+    system("cls");
+
+    while (true)
+    {
+        cord.X = 0;
+        cord.Y = 0;
+
+        SetConsoleCursorPosition(hand, cord);
+
+        SetConsoleTextAttribute(hand, 15);
+
+        cout << "B: ";
+
+        for (int i = 0; i < B.size(); i++)
+        {
+            cout << B[i] << " ";
+        }
+
+        cout << endl;
+
+        cout << "S: ";
+
+        for (int i = 0; i < S.size(); i++)
+        {
+            cout << S[i] << " ";
+        }
+
+        cout << endl;
+
+        cout << "field (file name / random): " << temp << endl;
+
+        cout << "length: " << x << endl;
+        cout << "width: " << y << endl;
+
+        cout << "generation: " << GenCount << endl;
+
+        FieldOutput(field, x, y);
+
+        NewGeneration(field, B, S, x, y);
+
+        GenCount++;
+    }
+}
